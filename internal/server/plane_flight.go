6ecab5e9d76da56e3c9222f978eb3da048e32c17
Arthur Abeilice <afa7789@gmail.com>
2022-04-05 08:18:54 -0300

    adding base request of web request

---
+++ internal/server/plane_flight.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func StartEndOfFlight(c *fiber.Ctx) error {
+	// body struct
+	b := struct {
+		Paths [][2]string `json:"paths"`
+	}{}
+
+	// parse the body
+	if err := c.BodyParser(&b); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(true)
+}
---
+++ internal/server/plane_flight_test.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestStartEndOfFlight(t *testing.T) {
+
+	tests := []struct {
+		name         string
+		payload      string
+		wantedStatus int
+	}{
+		{
+			name:         "Test with valid payload",
+			payload:      `{}`,
+			wantedStatus: fiber.StatusOK,
+		},
+		{
+			name:         "Test with invalid payload",
+			payload:      `{}`,
+			wantedStatus: fiber.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			s := NewServer()
+
+			req := httptest.NewRequest("GET", "http://google.com", nil)
+
+			resp, _ := s.router.Test(req)
+
+			// Do something with results:
+			if resp.StatusCode == tt.wantedStatus {
+				t.Errorf("StartEndOfFlight() = %v, want %v", resp.StatusCode, tt.wantedStatus)
+			}
+		})
+	}
+}
