7558ee07225167d88825bd301508e59cb533eaf7
Arthur Abeilice <afa7789@gmail.com>
2022-04-05 07:47:23 -0300

    added webserver base

---
+++ internal/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// Server is the definition of a REST server based on Gin
+type Server struct {
+	router *fiber.App
+}
+
+// New returns a new server that takes advantage of zerolog for logging
+// and holds a reference to the app configuration
+func NewServer() *Server {
+	server := &Server{}
+
+	r := fiber.New()
+
+	server.router = r
+
+	return server
+}
+
+// Start starts the REST server
+// simple mocking pattern to switch port if one is already in use.
+func (s *Server) Start(port int) {
+	err := s.router.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		// Using this error treatment to try again on next port
+		if strings.Contains(err.Error(), "address already in use") {
+			fmt.Println("")
+			log.Printf("PORT ALREADY IN USE::%d", port)
+			port++
+			log.Printf("TRYING NEXT PORT:%d\n", port)
+			s.Start(port)
+		} else {
+			panic(err)
+		}
+	}
+}
